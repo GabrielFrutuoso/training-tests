b61eabd923d3c085f581a68f18323549f1c6d228
mackbiel03 <mackbiel03@gmail.com>
2024-10-29 12:15:45 +0000

    feat: add a page for a specific champion infos

---
+++ src/pages/ChampionScreen/ChampionScreen.spec.tsx
@@ -0,0 +1,15 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import ChampionScreen from "./ChampionScreen";
+
+describe("champion page", () => {
+
+    test("should render champion page", () => {
+        const { getByTestId } = render(<ChampionScreen />)
+
+        expect(getByTestId('champion')).toBeInTheDocument()
+    })
+})
+
+
---
+++ src/pages/ChampionScreen/ChampionScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChampionScreen: React.FC = () => {
+  return (
+    <div data-testid="champion">
+      ChampionScreen
+    </div>
+  );
+};
+
+export default ChampionScreen;
