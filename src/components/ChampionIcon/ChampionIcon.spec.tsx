aca6b4b07e58683008242ee2620e13e6499b2b14
GabrielFrutuoso <mackbiel03@gmail.com>
2024-10-29 10:54:00 -0300

    fix: fixed test

--- src/components/ChampionIcon/ChampionIcon.spec.tsx
+++ src/components/ChampionIcon/ChampionIcon.spec.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { ChampionIcon } from "./ChampionIcon";
 
@@ -7,20 +7,22 @@ const component = <ChampionIcon name="Aatrox" icon="https://ddragon.leagueoflege
 
 describe('champions card', () => {
     test('should render', () => {
-        const {getByTestId} = render(component)
+        render(component)
 
-        expect(getByTestId('champions card')).toBeInTheDocument()
+        expect(screen.getByTestId('champions card')).toBeInTheDocument()
     })
 
     test('should render icon', () => {
-        const {getByRole} = render(component)
+        render(component)
 
-        expect(getByRole('img')).toBeInTheDocument()
+        expect(screen.getByRole('img')).toBeInTheDocument()
     })
 
     test('should render name', () => {
-        const {getByTitle} = render(component)
+        render(component)
 
-        expect(getByTitle('Aatrox')).toBeInTheDocument()
+        expect(screen.getByTitle('Aatrox')).toBeInTheDocument()
     })
+
+    
 })
