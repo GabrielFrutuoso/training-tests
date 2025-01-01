f4688b3b8f0d351048a1f6cfd89affa763ddcb0c
GabrielFrutuoso <mackbiel03@gmail.com>
2025-01-01 10:36:36 -0300

    test: create championScreen page tests

--- src/pages/ChampionScreen/ChampionScreen.spec.tsx
+++ src/pages/ChampionScreen/ChampionScreen.spec.tsx
@@ -1,15 +1,14 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
-import ChampionScreen from "./ChampionScreen";
+import { ChampionScreen } from "./ChampionScreen";
+
 
 describe("champion page", () => {
+  test("should render champion page", () => {
+    const { getByTestId } = render(<ChampionScreen />);
 
-    test("should render champion page", () => {
-        const { getByTestId } = render(<ChampionScreen />)
+    expect(getByTestId("champion")).toBeInTheDocument();
+  });
 
-        expect(getByTestId('champion')).toBeInTheDocument()
-    })
-})
-
-
+});
