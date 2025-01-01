6669ab45fdd4b025bef467b657637948329cc30b
GabrielFrutuoso <mackbiel03@gmail.com>
2025-01-01 11:36:21 -0300

    test:  add splash art, name and champion title tests

--- src/pages/ChampionScreen/ChampionScreen.spec.tsx
+++ src/pages/ChampionScreen/ChampionScreen.spec.tsx
@@ -1,14 +1,34 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { ChampionScreen } from "./ChampionScreen";
-
 
 describe("champion page", () => {
   test("should render champion page", () => {
     const { getByTestId } = render(<ChampionScreen />);
 
     expect(getByTestId("champion")).toBeInTheDocument();
+  });
+
+  test("should render champion splash art", async () => {
+    const searchParams = new URLSearchParams("?id=Aatrox&skin=2");
+    window.history.pushState({}, "", `?${searchParams.toString()}`);
+
+    const { getByAltText } = render(<ChampionScreen />);
+    const splashArt = getByAltText("splash art of Aatrox");
+    expect(splashArt).toHaveAttribute(
+      "src",
+      "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_2.jpg"
+    );
+  });
+
+  test("should render champion name and title", async () => { 
+    const searchParams = new URLSearchParams("?id=Aatrox&skin=0");
+    window.history.pushState({}, "", `?${searchParams.toString()}`);
+
+    const { getByText } = render(<ChampionScreen />);
+    waitFor(() => expect(getByText("Aatrox")).toBeInTheDocument());
+
   });
 
 });
