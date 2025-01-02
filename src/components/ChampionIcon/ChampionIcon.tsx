1f689e250d54336072139739830d4c9df9454822
GabrielFrutuoso <gaalexandre@leroymerlin.com.br>
2025-01-02 10:37:45 -0300

    fix: renamed Alt

--- src/components/ChampionIcon/ChampionIcon.tsx
+++ src/components/ChampionIcon/ChampionIcon.tsx
@@ -6,7 +6,7 @@ interface Props {
 export const ChampionIcon = ({ name, icon }: Props) => {
   return (
     <div data-testid="champions card" title={name} className="flex flex-col">
-      <img src={icon} alt={`icone do ${name}`} />
+      <img src={icon} alt={`icon of ${name}`} />
       <h1 className="text-neutral-300 ml-2">{name}</h1>
     </div>
   );
