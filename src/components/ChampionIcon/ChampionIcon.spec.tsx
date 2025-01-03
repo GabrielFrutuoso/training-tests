30e098c0061593ca709e07baedb1364f2194c267
GabrielFrutuoso <gaalexandre@leroymerlin.com.br>
2025-01-03 09:48:15 -0300

    test: testing if the link is redirecting correctly

--- src/components/ChampionIcon/ChampionIcon.spec.tsx
+++ src/components/ChampionIcon/ChampionIcon.spec.tsx
@@ -1,28 +1,52 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { ChampionIcon } from "./ChampionIcon";
-import { BrowserRouter } from "react-router-dom";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import { ChampionScreen } from "../../pages/ChampionScreen/ChampionScreen";
 
-const component = <BrowserRouter><ChampionIcon name="Aatrox" icon="https://ddragon.leagueoflegends.com/cdn/14.21.1/img/champion/Aatrox.png" /></BrowserRouter>
+const component = (
+  <MemoryRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <ChampionIcon
+            id="Aatrox"
+            name="Aatrox"
+            icon="https://ddragon.leagueoflegends.com/cdn/14.21.1/img/champion/Aatrox.png"
+          />
+        }
+      />
+      <Route path="/ChampionScreen" element={<ChampionScreen />} />
+    </Routes>
+  </MemoryRouter>
+);
 
-describe('champions card', () => {
-    test('should render', () => {
-        render(component)
+describe("champions card", () => {
+  test("should render", () => {
+    render(component);
 
-        expect(screen.getByTestId('champions card')).toBeInTheDocument()
-    })
+    expect(screen.getByTestId("champions card")).toBeInTheDocument();
+  });
 
-    test('should render icon', () => {
-        render(component)
+  test("should render icon", () => {
+    render(component);
 
-        expect(screen.getByRole('img')).toBeInTheDocument()
-    })
+    expect(screen.getByRole("img")).toBeInTheDocument();
+  });
 
-    test('should render name', () => {
-        render(component)
+  test("should render name", () => {
+    render(component);
 
-        expect(screen.getByTitle('Aatrox')).toBeInTheDocument()
-    })
+    expect(screen.getByTitle("Aatrox")).toBeInTheDocument();
+  });
 
-})
+  test("should redirect to champion screen", () => {
+    render(component);
+    fireEvent.click(screen.getByText("Aatrox"));
+
+    expect(screen.getByTestId("champion")).toBeInTheDocument();
+  });
+
+});
