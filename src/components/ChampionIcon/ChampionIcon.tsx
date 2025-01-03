f7bf2c21ad610763f8c2950e9c957a843a76f9fb
GabrielFrutuoso <gaalexandre@leroymerlin.com.br>
2025-01-03 09:47:41 -0300

    fix: use React-router-dom-directly in ChampionIcon

--- src/components/ChampionIcon/ChampionIcon.tsx
+++ src/components/ChampionIcon/ChampionIcon.tsx
@@ -1,13 +1,16 @@
+import { Link } from "react-router-dom";
+
 interface Props {
+  id: string;
   name: string;
   icon: string;
 }
 
-export const ChampionIcon = ({ name, icon }: Props) => {
+export const ChampionIcon = ({ id, name, icon }: Props) => {
   return (
-    <div data-testid="champions card" title={name} className="flex flex-col">
+    <Link to={`/ChampionScreen?id=${id}&skin=0`} data-testid="champions card" title={name} className="flex flex-col">
       <img src={icon} alt={`icon of ${name}`} />
       <h1 className="text-neutral-300 ml-2">{name}</h1>
-    </div>
+    </Link>
   );
 };
