6da58861f30abd883da419c063bd6e1db40d7000
GabrielFrutuoso <gaalexandre@leroymerlin.com.br>
2025-01-06 09:23:45 -0300

    fix: fixed render page test

--- src/pages/ChampionScreen/ChampionScreen.spec.tsx
+++ src/pages/ChampionScreen/ChampionScreen.spec.tsx
@@ -2,10 +2,11 @@ import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { ChampionScreen } from "./ChampionScreen";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 describe("champion page", () => {
   test("should render champion page", () => {
-    const { getByTestId } = render(<ChampionScreen />);
+    const { getByTestId } = render(<MemoryRouter><Routes><Route path="/" element={<ChampionScreen />}/> </Routes></MemoryRouter>);
 
     expect(getByTestId("champion")).toBeInTheDocument();
   });
