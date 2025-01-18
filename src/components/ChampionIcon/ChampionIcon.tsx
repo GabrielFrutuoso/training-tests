59a9ee73b122e64206e0069578f60b95d5b1508a
GabrielFrutuoso <mackbiel03@gmail.com>
2025-01-18 20:29:26 -0300

    test: add a role

--- src/components/ChampionIcon/ChampionIcon.tsx
+++ src/components/ChampionIcon/ChampionIcon.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const ChampionIcon = ({ id, name, icon }: Props) => {
   return (
-    <Link to={`/ChampionScreen?id=${id}&skin=0`} data-testid="champions card" title={name} className="flex flex-col">
+    <Link role="listitem" to={`/ChampionScreen?id=${id}&skin=0`} data-testid="champions card" title={name} className="flex flex-col">
       <img src={icon} alt={`icon of ${name}`} />
       <h1 className="text-neutral-300 ml-2">{name}</h1>
     </Link>
