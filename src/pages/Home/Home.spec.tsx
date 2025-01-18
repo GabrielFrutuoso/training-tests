ee0c7a2e4dbdc1ab98b3308270a7bc33ceeb21ac
GabrielFrutuoso <mackbiel03@gmail.com>
2025-01-18 20:29:06 -0300

    test: test list rendering

--- src/pages/Home/Home.spec.tsx
+++ src/pages/Home/Home.spec.tsx
@@ -1,13 +1,27 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { Home } from "./Home";
+import { BrowserRouter } from "react-router-dom";
 
+const renderWithRouter = (component: React.ReactNode) => {
+    return render(
+        <BrowserRouter>
+            {component}
+        </BrowserRouter>
+    );
+};
 
 describe('Home page', () => {
     test("should render home page", () => {
-        const { getByTestId } = render(<Home />)
+        const { getByTestId } = renderWithRouter(<Home />);
+        expect(getByTestId('home')).toBeInTheDocument();
+    });
 
-        expect(getByTestId('home')).toBeInTheDocument()
-    })
-})
+    test("should render list of champions", async () => {
+        renderWithRouter(<Home />);
+        const listItems = await screen.findAllByRole('listitem');
+        expect(listItems).toHaveLength(168);
+        
+    });
+});
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -15,15 +15,18 @@ export const Home = () => {
     });
   }, []);
 
+  console.log(champions);
+
   return (
     <div className="h-screen relative flex justify-center items-center">
       <div
         data-testid="home"
         className={`absolute top-0 right-0 h-screen w-screen bg-[url('https://www.lolvvv.com/_next/image?url=https%3A%2F%2Fwww.lolvvv.com%2Fwp-content%2Fuploads%2F2022%2F07%2Fsummoners-rift.jpg&w=1200&q=75')] bg-no-repeat bg-cover bg-center brightness-50 -z-10`}
       ></div>
-      <div className="grid grid-cols-8 gap-2 h-[80vh] overflow-y-auto">
+      <div role="list" className="grid grid-cols-8 gap-2 h-[80vh] overflow-y-auto">
         {champions.map((champion) => (
           <ChampionIcon
+            key={champion.id}
             id={champion.id}
             name={champion.name}
             icon={`https://ddragon.leagueoflegends.com/cdn/14.21.1/img/champion/${champion.id}.png`}
